c1aff1f14d571f84f10a75bb68f8a2a986b8d3d4
Yeonok Jo <>
2021-06-22 13:51:12 +0900

    mobile gnb 적용

--- js/custom.js
+++ js/custom.js
@@ -17,13 +17,13 @@ $gnb_li.on("mouseleave ", function () {
 });
 
 $gnb_sub.on("mouseenter", function(){
-  $(this).find(".left ul").animate({marginLeft: "260"}, 800);
-  $(this).find(".right-img").addClass("on");
+  // $(this).find(".left ul").animate({marginLeft: "260"}, 800);
+  $(this).find(".left-img").addClass("on");
 });
 
 $gnb_li.on("mouseleave", function(){
-  $(this).find(".left ul").animate({marginLeft: 0});
-  $(this).find(".right-img").removeClass("on");
+  // $(this).find(".left ul").animate({marginLeft: 0});
+  $(this).find(".left-img").removeClass("on");
 });
 
 // $gnb_sub.on("mouseleave", function(){
@@ -44,3 +44,50 @@ $gnb_li.each(function (index) {
     $gnb_li.eq(index).find(".sub").hide();
   });
 });
+
+const btnCall = document.querySelector('.btnCall');
+const menuMo = document.querySelector('.menuMo');
+
+// btnCall.addEventListener('click', e => {
+//   menuMo.classList.toggle('on')
+// })
+btnCall.onclick = function () { 
+  menuMo.classList.toggle("on");
+}
+
+window.onresize = function(e){ 
+  let wid = window.innerWidth; 
+
+  if(wid>=1180){ 
+    menuMo.classList.remove("on"); 
+  }
+}
+
+const items = document.querySelectorAll('.process li');
+const aside = document.querySelector('aside');
+// const close = aside.querySelector('span');
+
+for (let el of items) {
+    el.addEventListener('mouseenter', e => {
+        e.currentTarget.querySelector('a').style.display = "block";
+      });
+
+      el.addEventListener('mouseleave', e => {
+        e.currentTarget.querySelector('a').style.display = "none";
+      });
+
+    el.addEventListener('click', e => {
+      e.preventDefault();
+      const tit = e.currentTarget.querySelector('h2').innerText;
+      const txt = e.currentTarget.querySelector('p').innerText;
+  
+      aside.querySelector('h2').innerText = tit;
+      aside.querySelector('p').innerText = txt;
+
+      aside.classList.add('on');
+    });
+  }
+
+// close.addEventListener('click', e => {
+//   aside.classList.remove('on');
+// })  
